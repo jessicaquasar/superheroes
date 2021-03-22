eb18f439d16a6c6da2b360e2a5c374c27354672e
jessicaquasar <jessica.quasar@gmail.com>
2021-03-22 00:31:50 -0300

    feat: Mantem itens na localStorage e ordena lista

--- src/components/list.component.js
+++ src/components/list.component.js
@@ -1,53 +1,76 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { GetHero } from "../services/hero";
-import { List, HeartButton } from "./list.style";
+import { ErrorAlert } from "./error.component.js";
+import { List, HeartButton, CheckBoxWrapper, CheckBox, CheckBoxLabel, Sort } from "./list.style";
+import superhero from "../images/superhero.png";
 import { ReactComponent as Heart } from "../images/heart.svg";
 
 export function HeroesList({heroName}) {
 
-  const { hero }  = GetHero({heroName})
+  const { hero }  = GetHero({heroName});
   const [heroesList, setHeroesList] = useState([]);
   const [favourite, setFavourite] = useState(false); 
   const [sort, setSort] = useState(false);
-  const [sortList, setSortList] = useState([])
+  const [list, setList] = useState([]);
+  const [sortList, setSortList] = useState([]);
 
   useEffect(() => { 
-    if ( hero?.code === 200 && hero?.data?.count > 0) setHeroesList(heroesList => [...heroesList, hero?.data?.results[0]]);
+    if ( hero?.code === 200 && hero?.data?.count > 0) {
+      setList(list => [...list, hero?.data?.results[0]]);
+    }; 
   }, [hero, hero?.code]);
 
-  function orderHeroes(){
+  useEffect(() => {
+    const newList = [...list];
+    setHeroesList(newList.map(result => ({ ...result, favourite: "false" })))
+  }, [list]);
+
+  useEffect(() => {
+    let storedNames = JSON.parse(localStorage.getItem("heroes"));
+    setHeroesList(storedNames);
+  },[])
+  
+  useEffect(() => {
+    localStorage.setItem("heroes", JSON.stringify([...heroesList]));
+  },[heroesList]);
+
+  function sortHeroes(){
     const sorted = [...heroesList];
     setSortList(sorted.sort((a,b) =>
     (a.name > b.name) ? 1 : -1 ));
   };
-
-  // useEffect(() => {
-  //   localStorage.setItem("heroes", JSON.stringify({heroesList}));
-  // },[heroesList]);
   
   return(
     <>
-      <div>
-        <button type="button" onClick={() => {setSort(prev => !prev); orderHeroes()}}>Ordenar</button>
-      </div>
+      <Sort>
+        <img src={superhero} alt="herói" />
+        <label>Ordenar por nome - A/Z</label>
+        <CheckBoxWrapper>
+          <CheckBox id="checkbox" type="checkbox" onClick={() => {setSort(prev => !prev); sortHeroes()}} />
+          <CheckBoxLabel htmlFor="checkbox" />
+        </CheckBoxWrapper>
+      </Sort>
+      {/* {notFound ? (
+        <ErrorAlert text="Herói não encontrado" />
+      ): null} */}
       <List>
         {sort ? (
-          sortList.map((item, index) => 
+          sortList.map((item,i) => 
             <li key={item?.id} tabIndex="0">
               <Link to={{ pathname:`/hero/${item?.name}`, state: {name: item?.name, id: item?.id}}}>
                 <img src={`${item?.thumbnail?.path}.${item?.thumbnail?.extension}`} alt={item?.name}/>
               </Link>
               <div>
                 <p>{item?.name}</p>
-                <HeartButton type="button" onClick={() => setFavourite(prev => !prev)} favourite={favourite}>
+                <HeartButton type="button" onClick={() => {setFavourite(item => !item.favourite[i])}} favourite={favourite}>
                   <Heart/>
-                </HeartButton>
+                </HeartButton>  
               </div>
             </li>
           )
         ) : (
-          heroesList.map((item, index) => 
+          heroesList.map((item) => 
             <li key={item?.id} tabIndex="0">
               <Link to={{ pathname:`/hero/${item?.name}`, state: {name: item?.name, id: item?.id}}}>
                 <img src={`${item?.thumbnail?.path}.${item?.thumbnail?.extension}`} alt={item?.name}/>
--- src/components/list.style.js
+++ src/components/list.style.js
@@ -46,3 +46,63 @@ export const HeartButton = styled.button`
     }
   }
 `;
+
+export const Sort = styled.div`
+  align-items: center; 
+  color: #ff1510;
+  display: flex;
+  justify-content: flex-end;
+  margin: 0 8px;
+
+  img, label {
+    margin-right: 16px;
+  }
+`;
+
+// reference: https://codesandbox.io/s/6v7n1vr8yn?file=/src/index.js:308-1129
+
+export const CheckBoxWrapper = styled.div`
+  position: relative;
+`;
+
+export const CheckBoxLabel = styled.label`
+  background: #b9b9b9;
+  border-radius: 15px;
+  cursor: pointer;
+  height: 26px;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 42px;
+  &::after {
+    background-color: #ff1510;
+    border-radius: 50%;
+    box-shadow: 1px 3px 3px 1px rgba(0, 0, 0, 0.2);
+    content: "";
+    display: block;
+    height: 18px;
+    margin: 3px;
+    transition: 0.2s;
+    width: 18px;
+  }
+`;
+
+export const CheckBox = styled.input`
+  border-radius: 15px;
+  height: 26px;
+  opacity: 0;
+  width: 42px;
+  z-index: 1;
+  &:checked + ${CheckBoxLabel} {
+    &::after {
+      background-color: #ff1510;
+      border-radius: 50%;
+      content: "";
+      display: block;
+      height: 18px;
+      margin-left: 21px;
+      transition: 0.2s;
+      width: 18px;
+    }
+  }
+`;
